fedd4d908924fc1e75a87bdab0e99d01b9faab4b
Peter Gasston <peter@rehabstudio.com>
2015-05-27 21:31:24 +0100

    Disable buggy autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ var debug = require('gulp-debug');
 gulp.task('stylesheets', function () {
   gulp.src('src/index.css')
   .pipe(cssnext({
-      compress: true
+      compress: false,
+      features: {
+        autoprefixer: false
+      }
   }))
   .pipe(debug({title: 'unicorn:'}))
   .pipe(gulp.dest('dist'));
